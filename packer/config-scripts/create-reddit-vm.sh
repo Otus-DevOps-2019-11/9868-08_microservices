093a64b835855808e093cd703197001446252cb7
Roman <26068728+9868-08@users.noreply.github.com>
2020-01-10 15:31:39 +0400

    Update create-reddit-vm.sh

--- packer/config-scripts/create-reddit-vm.sh
+++ packer/config-scripts/create-reddit-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
+gcloud config set project  infra-986808
 gcloud compute instances create reddit-gcloud-script \
     --image reddit-base-1578467159 \
     --image-project forward-karma-262407
-gcloud config set project  forward-karma-262407
