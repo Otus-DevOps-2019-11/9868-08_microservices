73cfd31fc7974099ad3cf11b0cf208b42a9dedbb
9868-08 <9868.08@gmail.com>
2020-01-25 09:30:20 +0400

    all files from all branches

--- terraform-2/prod/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "ar85-tf-state-prod"
-    prefix = "terraform/state"
-  }
-}
---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,20 @@
+#provider "google" {
+#  version = "~> 2.15"
+#  project = var.project
+#  region  = var.region
+#}
+
+
+
+module "storage-bucket" {
+  source  = "SweetOps/storage-bucket/google"
+  version = "0.3.0"
+  name = "storage-bucket-9868.08"
+  location      = "europe-west1"
+  force_destroy = true
+}
+
+output storage-bucket_url {
+  value = module.storage-bucket.url
+}
+
