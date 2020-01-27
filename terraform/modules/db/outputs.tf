86a03adb1d32eaac95293982fdecad0532fa71f0
9868-08 <9868.08@gmail.com>
2020-01-27 11:41:13 +0400

    1

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -3,5 +3,5 @@ output "db_internal_ip" {
 }
 
 output "db_external_ip" {
-  value = "${google_compute_instance.db.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.db.network_interface.0.access_config.0.nat_ip}"
 }
